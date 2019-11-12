23436de0a78678405980d4fb5335d13d2b17ca90
RoussPat <patrickrouss@gmail.com>
2019-11-12 18:18:10 +0100

    implementation de fonction dans flow.ml (pas fini) et netoyage du projet avec des nouveaux floders

--- flow.ml
+++ flow.ml
@@ -1,5 +1,10 @@
 open Graph
 
+type labledId = {
+  id:id;
+  bool:marked;
+}
+
 (*renvoi true si l contient n*)
 let contains l n = 
   let rec loop = function
@@ -7,6 +12,39 @@ let contains l n =
     |_ -> false
   in loop l
 
+let labelfind l id = 
+  let rec inner l = match l with
+    |x::rest -> if (id=x.id) then x else inner l
+    |_ -> false
+  in 
+  inner l ;;
+
+let unlabel labeledlist = 
+  let inner acu l =
+    match l with 
+    |[] -> acu
+    |x::rest -> inner x.id::acu rest
+  in
+  inner [] labeledlist ;;
+
+let voisin g id = (* 'a graph -> 'a' -> 'a' list *)
+  (* retourne une liste de touts les voisins d'un node id dans le graph g
+    parcours tout les arcs de g avec G.iter 
+    verifie avec un predicat bidon (fun id1 id2 -> id1 = id) prend un arc et verifie si la source est id
+    (on peut utiliser List.cons sur le iter mais il faut raouter la dimention du predicat )
+
+    TO DO
+  *)
+
+let rmmarked idlist labeledlist = (*Pirvate*)
+  (* retire toute les occurences de noeux marqués dans labeledlist de la list idlist et la retourne*)
+  let rec inner acu idlist = 
+    match idlist with
+      |[] -> acu
+      |x::rest -> (let k = labelfind labeledlist x in 
+        if k.marked then inner acu rest else inner (x::acu) rest)
+    in
+  inner [] idlist ;;
 
 
 let find_path g forbidden id1 id2  = ()
@@ -19,4 +57,24 @@ let find_path g forbidden id1 id2  = ()
   le premier chemin trouvé ? le plus court (nb de nodes)? pb au moment du dépilement
 
   let rec find_next acu last = ??
+  
+  il faut une pile,
+  il faut labeliser les sommets ou utiliser une liste d'assotiation
+  entre les sommets et des labels (liste de LabeledId)
+  ensuite on empile le permier sommet du graph
+  et tant que la pile n'est pas vide on explore la tete de la fille 
+  en enfilan les fils de chaques noeux si ils ne sont pas marqués
+  
+  on retourne l'etat du lablel de destination a la fin de l'algo
+
+
+  IMPROUMENTS: on arrete si on explore id2
     *)
+    let lablist = (* ...*) in
+
+    let rec explorer id = 
+    lablist[id].marked = true ; 
+
+    Pile.push (rmmarked (voisin g id));;
+
+
--- flow.mli
+++ flow.mli
@@ -3,10 +3,12 @@ open Graph
 (* A path is a list of nodes. *)
 type path = id list
 
+type labledId
 (* find_path gr forbidden id1 id2 
  *   returns None if no path can be found.
  *   returns Some p if a path p from id1 to id2 has been found. 
  *
  *  forbidden is a list of forbidden nodes (they have already been visited)
  *)
+val voisin: 'a graph -> 'a' -> 'a' list 
 val find_path: int graph -> id list -> id -> id -> path option
